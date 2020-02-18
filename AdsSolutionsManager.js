fee9f022b77e3b0f16163d428f90804393caef19
archist <hi.cyril@gmail.com>
2020-02-18 22:53:02 +0100

    GAds solutions, created 2 objects: single and manager

---
+++ AdsSolutionsManager.js
@@ -0,0 +1,25 @@
+class AdsSolutionsSingle{
+  constructor(){
+
+  }
+  reports(){
+    const funcs = {
+
+    }
+  }
+  bidding(){
+    const funcs = {
+
+    }
+  }
+  tools(){
+    const funcs = {
+
+    }
+  }
+  alerts(){
+    const funcs = {
+
+    }
+  }
+}
---
+++ AdsSolutionsSingle.js
@@ -0,0 +1,27 @@
+class AdsSolutionsSingle{
+  constructor(spreadsheetId){
+    if(typeof spreadsheetId == "undefined")spreadsheetId = "1BzXxm5-n4N0Y3kHMyijyc2LRqKZPXH-57FFKPZ-E2Ik"
+    const spreadsheetUrl = "https://docs.google.com/spreadsheets/d/"+spreadsheetId
+    Logger.log("spreadsheetId: %s\nspreadsheetUrl: %s", spreadsheetId, spreadsheetUrl)
+  }
+  reports(){
+    const funcs = {
+
+    }
+  }
+  bidding(){
+    const funcs = {
+
+    }
+  }
+  tools(){
+    const funcs = {
+
+    }
+  }
+  alerts(){
+    const funcs = {
+
+    }
+  }
+}
